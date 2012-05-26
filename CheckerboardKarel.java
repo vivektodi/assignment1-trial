364d74e0403af68189135b5d89c47a737921a1a6
S <s>
2012-05-26 21:08:06 +0530

    {"type":"TI", "runs":"1"}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -20,6 +20,7 @@ public class CheckerboardKarel extends SuperKarel {
 	private void startSolving()
 	{
 		fillFirstAndWalk();
+		while ((notFacingEast() && frontIsClear() && leftIsClear()) || (notFacingWest() && frontIsClear() && rightIsClear()))
 	}
 	
 	
